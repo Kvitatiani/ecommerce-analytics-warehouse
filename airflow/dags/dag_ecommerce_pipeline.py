7849f789e5cf16078c7c5f17e68ee1f2aefaf82e
Aleksi Kvitatiani <aleksi.kvitatiani@nexus.ge>
2026-04-05 21:00:27 +0400

    Add orchestration with airflow, create 1st schedule

---
+++ airflow/dags/dag_ecommerce_pipeline.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+sys.path.insert(0, '/opt/airflow')
+
+from src.ingestion.bronze_loader import insert_data_to_bronze, table_config, source
+from src.ingestion.api_client import get_api_data
+from src.transformations.bronze_to_silver import insert_data_to_silver, sql_files as silver_sql_files
+from src.transformations.silver_to_gold import insert_data_to_gold, sql_files as gold_sql_files
+
+def run_bronze_ingestion():
+    for table in table_config:
+        data_json = get_api_data(table["endpoint"])
+        if data_json:
+            insert_data_to_bronze(data_json, table["table_name"], table["payload_column"], source)
+
+def run_silver_ingestion():
+    insert_data_to_silver(silver_sql_files)
+
+def run_gold_ingestion():
+    insert_data_to_gold(gold_sql_files)
+
+
+with DAG(
+    dag_id='ecommerce_pipeline',
+    start_date=datetime(2026, 4, 1),
+    schedule='@daily',
+    catchup=False
+) as dag:
+    # define tasks here
+    bronze_task = PythonOperator(
+        task_id='bronze_ingestion',
+        python_callable=run_bronze_ingestion
+    )
+
+    silver_task = PythonOperator(
+        task_id='silver_ingestion',
+        python_callable=run_silver_ingestion
+    )
+    gold_task = PythonOperator(
+        task_id='gold_ingestion',
+        python_callable=run_gold_ingestion
+    )
+
+    bronze_task >> silver_task >> gold_task
